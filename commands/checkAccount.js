688b33bf0f5a8a5cd77777023df0d57bc75fe1a1
limitpointinf0 <coneal0523@protonmail.com>
2021-08-03 20:02:08 +0700

    [ADD] format to check balance

--- commands/checkAccount.js
+++ commands/checkAccount.js
@@ -32,10 +32,15 @@ function check () {
     const status = new Spinner('Checking account, please wait...');
     status.start();
     checkAccounts(accountAddress)
-        .then((accounts) => {
-            console.log('\n')
-            console.log(util.inspect(accounts, false, null))
+        .then((account) => {
             status.stop();
+            console.log('\n')
+            console.log(chalk.yellowBright(`Account ID: ${account.accountId}\n`))
+            account.balances.forEach((balance) => {
+                console.log(chalk.yellowBright(`Type: ${balance.type}`))
+                console.log(chalk.yellowBright(`Balance: ${balance.balance}`))
+                console.log('\n')
+            });
         })
         .catch((e) => { 
             status.stop();
