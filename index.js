abc8726fb997239fc74643c6ae0e2d68cca3377e
limitpointinf0 <coneal0523@protonmail.com>
2021-08-07 15:19:07 +0700

    [ADD] rename directory and reorder commands

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 #! /usr/bin/env node
 
 const { program } = require('commander')
-const check = require('./commands/checkAccount')
-const txn = require('./commands/makeTransfer')
-const purchase = require('./commands/purchaseToken')
-const trustline = require('./commands/setTrustline')
-const create = require('./commands/createAccount')
-const set = require('./commands/setAccount')
-const unset = require('./commands/unsetAccount')
-const stream = require('./commands/streamActivity')
+const check = require('./src/checkAccount')
+const txn = require('./src/makeTransfer')
+const purchase = require('./src/purchaseToken')
+const trustline = require('./src/setTrustline')
+const create = require('./src/createAccount')
+const set = require('./src/setAccount')
+const unset = require('./src/unsetAccount')
+const stream = require('./src/streamActivity')
+
+program
+    .command('set')
+    .description('Set an Account')
+    .action(set)
+
+program
+    .command('unset')
+    .description('Unset an Account')
+    .action(unset)
 
 program
     .command('check')
@@ -21,29 +31,19 @@ program
     .action(txn)
 
 program
-    .command('purchase')
-    .description('Purchase an Asset')
-    .action(purchase)
-
-program
     .command('trust')
     .description('Create Trustline')
     .action(trustline)
 
 program
+    .command('purchase')
+    .description('Purchase an Asset')
+    .action(purchase)
+
+program
     .command('create')
     .description('Create an Account')
     .action(create)
-
-program
-    .command('set')
-    .description('Set an Account')
-    .action(set)
-
-program
-    .command('unset')
-    .description('Unset an Account')
-    .action(unset)
 
 program
     .command('stream')
