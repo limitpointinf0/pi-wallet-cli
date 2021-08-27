a94fa26ef7c12b92fe2c30175056384b828b803e
limitpointinf0 <coneal0523@protonmail.com>
2021-08-28 02:03:26 +0700

    [ADD] remove unused imports

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
-const conf = new (require('conf'))()
 const config = require('../config.json');
 const prompt = require('prompt-sync')({ sigint: true });
 const chalk = require('chalk')
 var fs = require('fs');
-var StellarBase = require('stellar-base');
 var path = require('path');
 var appDir = path.dirname(require.main.filename);
 
@@ -11,9 +9,14 @@ function configure() {
 
     const server = prompt(chalk.yellowBright(`Server: `));
     const passphrase = prompt(chalk.yellowBright(`Network Passphrase: `));
+    const currency = prompt(chalk.yellowBright(`Currency: `));
+    const baseReserve = prompt(chalk.yellowBright(`Base Reserve: `));
 
     config.server = server;
     config.networkPassphrase = passphrase;
+    config.currency = currency;
+    config.baseReserve = baseReserve;
+
     var json = JSON.stringify(config);
     fs.writeFile(appDir + '/config.json', json, 'utf8', (err) => {
         if (err) throw err;
